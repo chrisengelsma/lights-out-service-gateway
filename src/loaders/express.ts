0bdabe8feb54ea06cc97d071c099fae130aec6fe
Chris Engelsma <chris.engelsma@gmail.com>
2023-02-25 20:04:07 -0600

    Update

--- src/loaders/express.ts
+++ src/loaders/express.ts
@@ -10,6 +10,8 @@ export default ({ app }: { app: express.Application }) => {
 
   const corsOptions: any = {
     origin: [
+      'http://localhost:8080',
+      'http://127.0.0.1:8080',
       'http://localhost:3000',
       'http://127.0.0.1:3000',
       'https://lights-out.app',
